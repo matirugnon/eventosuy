59c04976e4ac44c9ddaf2837a99b5df238e2d5a8
Matías Eloy Rugnon Grottola <matias.rugnon@fing.edu.uy>
2025-08-27 15:09:03 -0300

    borre comentarios en IContUSuario, es prueba de merge

--- src/logica/Controladores/IControladorUsuario.java
+++ src/logica/Controladores/IControladorUsuario.java
@@ -12,13 +12,6 @@ import logica.DatatypesYEnum.DTFecha;
  */
 public interface IControladorUsuario {
 
-    /**
-     * Registra al usuario en el sistema.
-     * @param n Nombre del usuario.
-     * @param ap Apellido del usuario.
-     * @param ci Cédula del usuario.
-     * @throws UsuarioRepetidoException Si la cédula del usuario se encuentra registrada en el sistema.
-     */
 
 	public void altaUsuario(Usuario u);
 
@@ -26,19 +19,10 @@ public interface IControladorUsuario {
 
     public void altaOrganizador(String nick, String nombre, String correo, String descripcion, String link) throws UsuarioRepetidoException;
 
-    /**
-     * Retorna la información de un usuario con la cédula indicada.
-     * @param ci Cédula del usuario.
-     * @return Información del usuario.
-     * @throws UsuarioNoExisteException Si la cédula del usuario no está registrada en el sistema.
-     */
-    public abstract DataUsuario verInfoUsuario(String ci) throws UsuarioNoExisteException;
 
-    /**
-     * Retorna la información de todos los usuarios registrados en el sistema.
-     * @return Información de los usuarios del sistema.
-     * @throws UsuarioNoExisteException Si no existen usuarios registrados en el sistema.
-     */
+    //funcion que ya venia con ejemplo
+    public abstract DataUsuario verInfoUsuario(String ci) throws UsuarioNoExisteException;
+    //funcion que ya venia con ejemplo
     public abstract DataUsuario[] getUsuarios() throws UsuarioNoExisteException;
 
 
