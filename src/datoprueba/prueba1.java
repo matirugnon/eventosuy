6c614e3c5896e6c168f9c5fcc9bb658707cdd038
kels04 <agustinpereira2004@hotmail.com>
2025-08-31 16:44:27 -0300

    datosprueba

---
+++ src/datoprueba/prueba1.java
@@ -0,0 +1,14 @@
+package datoprueba;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class prueba1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
